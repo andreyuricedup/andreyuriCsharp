46d7951e9c74ce828ef5bd87c8ad06c4efc4d2c7
andreyuricedup <guilhermemoraess1611@gmail.com>
2025-05-22 09:17:43 -0300

    criacao do projeto cadastro jogos

---
+++ SoVetor/CadastroJogos/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CadastroJogos
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //DECLARAR I VETOR DE NOMES DE JOGOS
+            string[] vetJ = new string[3];
+
+            do
+            {
+                Console.Clear();
+                //MENU
+                Console.WriteLine(@"
+█▀▀ ▄▀█ █▀▄ ▄▀█ █▀ ▀█▀ █▀█ █▀█   █▀▄ █▀▀   ░░█ █▀█ █▀▀ █▀█ █▀
+█▄▄ █▀█ █▄▀ █▀█ ▄█ ░█░ █▀▄ █▄█   █▄▀ ██▄   █▄█ █▄█ █▄█ █▄█ ▄█");
+                Console.WriteLine("1 - Cadastrar");
+                Console.WriteLine("2 - Consultar");
+                Console.WriteLine("3 - Atualizar");
+                Console.WriteLine("4 - Excluir");
+                int opcao = int.Parse(Console.ReadLine());
+
+                if (opcao == 1)
+                {
+                    //CADASTRAR NOME NO VETOR
+                    for (int i = 0; i < vetJ.Length; i++)
+                    {
+                        Console.WriteLine($"Informe o {i + 1}° nome do jogo: ");
+                        vetJ[i] = Console.ReadLine();
+                    }
+                }
+                else if (opcao == 2)
+                {
+                    for (int i = 0; i < vetJ.Length; i++)
+                    {
+                        Console.WriteLine($"Posicao: {i} | Nome do jogo: {vetJ[i]}");
+                        
+                    }
+
+                    Console.WriteLine("Para continuar, pressione a tecla Enter ");
+                    Console.ReadKey();
+
+                }
+            } while (true);
+        }
+    }
+}
